b591d5cd2aa2da360465a0937a711a1c136df1b7
Danilo Kenzo <dani.kenzo@hotmail.com>
2019-05-20 15:54:01 -0300

    Sort com Lista imutavel, adicionando toString e tempoTotal

--- src/br/com/alura/TestaCurso2.java
+++ src/br/com/alura/TestaCurso2.java
@@ -1,19 +1,26 @@
 package br.com.alura;
 
+import java.util.Collections;
 import java.util.List;
 
-public class TestaCurso {
+public class TestaCurso2 {
 
 	public static void main(String[] args) {
 		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
 
 		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
 		javaColecoes.adiciona(new Aula("Criando uma aula", 20));
-		javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
+		javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
+		
+		List<Aula> aulasImutaveis = javaColecoes.getAulas();
+		System.out.println(aulasImutaveis);
 		
 		List<Aula> aulas = javaColecoes.getAulas();
 		
-		System.out.println(javaColecoes.getAulas());
-		System.out.println(javaColecoes.getTempoTotal());
+		Collections.sort(aulas);
+		System.out.println(aulas);
+		
+		
+		
 	}
 }
